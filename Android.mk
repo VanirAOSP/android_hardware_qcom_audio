c05f2992635ad8ee02043c6f7427e0b9acd09023
DHOMD <DHO@dho.im>
2016-06-30 08:55:07 -0400

    Make this a legacy branch
    Change-Id: I321c1517770d993a04e79f33d4210f578555f209

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
+ifeq ($(BOARD_USES_LEGACY_AUDIO),true)
+
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -8,6 +10,8 @@ ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
 include $(MY_LOCAL_PATH)/legacy/Android.mk
 else
 include $(MY_LOCAL_PATH)/hal/Android.mk
+endif
+
 endif
 
 endif

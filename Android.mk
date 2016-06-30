e393091081ef8907b6807b03dbdbf056faa96559
DHOMD <DHO@dho.im>
2016-06-30 09:13:20 -0400

    Make compatiable with legacy branch
    Change-Id: If99ce53e9f870bf3e472612e80dcdda92c5b3e1a

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
+ifeq ($(BOARD_USES_LEGACY_AUDIO),)
+
 ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -18,6 +20,8 @@ include $(MY_LOCAL_PATH)/policy_hal/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
 include $(MY_LOCAL_PATH)/audiod/Android.mk
 include $(MY_LOCAL_PATH)/post_proc/Android.mk
+endif
+
 endif
 
 endif
